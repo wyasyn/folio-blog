07e390aa714def3e7bcfd62159281b2c04876e11
Yasin Walum <ywalum@gmail.com>
2025-01-27 16:19:18 +0300

    blog page made

--- app/(root)/blog/[slug]/page.tsx
+++ app/(root)/blog/[slug]/page.tsx
@@ -1,4 +1,7 @@
+import NotFound from "@/app/not-found";
 import BackBtn from "@/components/BackBtn";
+import Categories from "@/components/CategoriesBadges";
+import { getBlogPostBySlug } from "@/lib/actions/posts";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 type Params = Promise<{ slug: string }>;
@@ -6,14 +9,27 @@ type Params = Promise<{ slug: string }>;
 export async function generateMetadata(props: { params: Params }) {
   const params = await props.params;
   const slug = params.slug;
+  const { error, blogPost } = await getBlogPostBySlug(slug);
+  if (error || !blogPost) NotFound();
+
   return {
-    title: slug,
+    title: blogPost?.title,
+    description: blogPost?.excerpt,
+    openGraph: {
+      title: blogPost?.title,
+      description: blogPost?.excerpt,
+      images: [{ url: blogPost?.image }],
+    },
   };
 }
 
 export default async function Page(props: { params: Params }) {
   const params = await props.params;
   const slug = params.slug;
+  const { error, blogPost } = await getBlogPostBySlug(slug);
+  if (error || !blogPost) NotFound();
+
+  const categories = blogPost?.categories;
 
   return (
     <div>
@@ -21,8 +37,19 @@ export default async function Page(props: { params: Params }) {
         <BackBtn />
       </div>
 
-      <div>
-        <MDXRemote source={slug} />
+      <div className="prose dark:prose-invert">
+        {blogPost?.body ? (
+          <MDXRemote
+            components={{
+              Categories: (props) => (
+                <Categories {...props} categories={categories} />
+              ),
+            }}
+            source={blogPost.body}
+          />
+        ) : (
+          <p>No content available.</p>
+        )}
       </div>
     </div>
   );
