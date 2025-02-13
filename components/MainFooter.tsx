5b2b03f7e076ce2407282ccef321f25ff0d6fe74
Yasin Walum <ywalum@gmail.com>
2025-02-13 22:29:10 +0300

    added clock

--- components/MainFooter.tsx
+++ components/MainFooter.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import { Clock } from "./clock";
 
 export default function MainFooter() {
   return (
     <div className=" mt-auto text-xs text-muted-foreground/50 py-4 w-full  flex items-center justify-between gap-5">
-      <p>{new Date().getFullYear()} - Yasin Walum. All rights reserved.</p>
-
+      <p>{new Date().getFullYear()} - Yasin Walum. All rights reserved.</p>{" "}
+      <Clock />{" "}
       <Link
         className="ml-auto underline hover:text-foreground duration-300 transition-all"
         href="/policy"
